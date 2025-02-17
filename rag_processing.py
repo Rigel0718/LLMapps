76b550be5ba7b327e23a08f917df52ff970abe61
geonhee <pollux0718@gmail.com>
2025-02-17 19:38:01 +0900

    Refactor RAG document processing with minor improvements and TODO comment

--- rag_processing.py
+++ rag_processing.py
@@ -1,22 +1,21 @@
 from langchain_community.document_loaders import PyPDFLoader, Docx2txtLoader, UnstructuredPowerPointLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_openai import ChatOpenAI
 from streamlit.runtime.uploaded_file_manager import UploadedFile
 from langchain.docstore.document import Document
 
 import tempfile
 import os
 from typing import List
-from io import BytesIO
-from langchain_community.document_loaders import (
-    PyPDFLoader, Docx2txtLoader, UnstructuredPowerPointLoader
-)
 
 def get_text(docs: List[UploadedFile]) -> List[Document]:
+    '''
+    TODO
+    splitter 유연성 높이기
+    '''
     doc_list = []
 
     for doc in docs:
-        file_bytes = doc.getvalue()  # 바이트 데이터 추출
+        file_bytes = doc.getvalue()  # 바이트 데이터 추출 (UploadedFile객체는 BytesIO 객체를 기본으로 한다)
         file_name = doc.name  # UploadedFile 객체라서 `.name` 사용 가능
 
         # 확장자 체크
@@ -31,16 +30,17 @@ def get_text(docs: List[UploadedFile]) -> List[Document]:
             loader_class = UnstructuredPowerPointLoader
         else:
             print(f"⚠️ Unsupported file type: {file_name}")
-            continue  # 지원되지 않는 파일은 건너뛰기
+            continue  # 지원되지 않는 파일 확장자는 건너뛰기
 
-        # 임시 파일 생성 및 저장
+        #임시 파일 생성 및 저장
         with tempfile.NamedTemporaryFile(delete=False, suffix=suffix) as tmp_file:
             tmp_file.write(file_bytes)
-            tmp_file_path = tmp_file.name  # 파일 경로 저장
+            tmp_file_path = tmp_file.name  # 임시 파일 경로 저장
 
         try:
             # 적절한 로더 사용
             loader = loader_class(tmp_file_path)
+            # splitter 사용 
             text_splitter = RecursiveCharacterTextSplitter(chunk_size = 900, chunk_overlap = 200)
             documents = loader.load_and_split(text_splitter=text_splitter)
             doc_list.extend(documents)
