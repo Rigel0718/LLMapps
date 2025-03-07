23d3f3c54bde41712789bf290ec3e71be3d775e0
geonhee <pollux0718@gmail.com>
2025-03-07 15:06:14 +0900

    Add FAISS vectorstore support for document embedding

--- rag_vectorstore.py
+++ rag_vectorstore.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
 from langchain_community.vectorstores import Chroma
+from langchain_community.vectorstores import FAISS
 from langchain_community.embeddings import OpenAIEmbeddings
 from langchain.docstore.document import Document
 from typing import List
@@ -15,4 +16,12 @@ def load_documents_chroma_vectorstore(docs : List[Document]) -> Chroma :
         collection_name=f"{str(time.time()).replace('.', '')[:14]}_" + st.session_state['session_id']
     )
     
+    return vector_db
+
+def load_documents_faiss_vectorsotre(docs : List[Document]) -> FAISS :
+    embedding = OpenAIEmbeddings(api_key=st.session_state.openai_api_key)
+    vector_db = FAISS.from_documents(
+        documents=docs,
+        embedding=embedding
+    )
     return vector_db
