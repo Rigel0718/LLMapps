a7c2ff2d13354a04043b44f0b9d70a13e78551f5
geonhee <pollux0718@gmail.com>
2025-03-07 15:42:22 +0900

    Add detailed docstrings to utility functions in utils.py

--- utils.py
+++ utils.py
@@ -4,7 +4,10 @@ from langchain.schema import AIMessage, HumanMessage
 
 
 def convert_chat_history(chat_messages):
-    """Streamlit의 세션 상태에서 저장된 채팅 기록을 LangChain 메시지 형식으로 변환"""
+    '''
+    Streamlit의 세션 상태에서 저장된 채팅 기록을 LangChain 메시지 형식으로 변환
+    '''
+    
     return [
         HumanMessage(content=msg["content"]) if msg["role"] == "user" 
         else AIMessage(content=msg["content"])
@@ -17,6 +20,9 @@ from langchain.chains.base import Chain
 
 
 def _pick_chain_output_(chain : Chain, messages, query=None):
+    '''
+    chain의 stream 출력을 따르면서, chatbot에 필요한 text형태의 key 값을 추출해주는 함수
+    '''
     
     if rag_available():
         return chain.pick("answer").stream({'messages': messages, 'input' : query})
@@ -26,6 +32,11 @@ def _pick_chain_output_(chain : Chain, messages, query=None):
     
 
 def stream_response(chain, messages, query=None):
+    '''
+    assistant가 stream형태의 답변을 구사할 수 있도록 만들어주는 함수
+
+    * chunk와 UI의 충돌 방지를 위한 time.sleep(0.05) 구현
+    '''
     with st.chat_message("assistant"):
         
         for chunk in _pick_chain_output_(chain,messages,query):
