6cfbd7a8d077ee45235ab752f7a36fe37d8d37a9
geonhee <pollux0718@gmail.com>
2025-03-26 20:45:42 +0900

    Add message history management with SQLite support in message_history.py

---
+++ message_history.py
@@ -0,0 +1,27 @@
+from langchain_community.chat_message_histories import SQLChatMessageHistory
+from langchain_core.runnables.utils import ConfigurableFieldSpec
+from langchain_core.runnables.history import RunnableWithMessageHistory
+
+def get_message_history_sqlitedb(client_id, conversation_num):
+    return SQLChatMessageHistory(
+        table_name=client_id,
+        session_id=conversation_num,
+        connection='sqlite:///.db'
+    )
+
+configs =[
+    ConfigurableFieldSpec(
+        id='client_id',
+        annotation=str,
+        name='Client ID',
+        default='',
+        is_shared=True
+    ),
+    ConfigurableFieldSpec(
+        id='conversation_num',
+        annotation=str,
+        name='Conversation_NUM',
+        default='',
+        is_shared=True
+    )
+]
