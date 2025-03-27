32ef9b5cbb994b36c1601a7f5d4d3d304c026d37
geonhee <pollux0718@gmail.com>
2025-03-27 12:29:18 +0900

    Implement base CHAT sys in Persist_CHAT.py

--- pages/Persist_CHAT.py
+++ pages/Persist_CHAT.py
@@ -1,9 +1,43 @@
 import streamlit as st
-import time
+from langchain_openai import ChatOpenAI
 from langchain_core.runnables.history import RunnableWithMessageHistory
 
 
 MODEL = ['gpt-4o-mini', 'o3-mini']
 
-st.set_page_config(page_title="Persist-CHAT")
 
+def main():
+    st.set_page_config(page_title="Persist-CHAT")
+
+    if 'llm' not in st.session_state:
+        st.session_state.llm = None
+
+    if 'openai_api_key' not in st.session_state:
+          st.session_state.openai_api_key = None
+
+    if 'client_id' not in st.session_state:
+          st.session_state.client_id = None
+
+    if 'conversation_num' not in st.session_state:
+          st.session_state.conversation_num = None
+
+    
+
+    with st.sidebar:
+        st.text_input("OpenAI API Key", key="openai_api_key", type="password")
+        "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
+        st.selectbox("🤖 Select a Model", options=MODEL, key = 'model')
+
+
+
+if 'messages' not in st.session_state:
+        st.session_state.messages = [{"role": "assistant", "content": "Hello, How can I help you?"}]
+
+for message in st.session_state.messages:
+        st.chat_message(message["role"]).write(message["content"])
+
+st.session_state.llm = ChatOpenAI(
+        api_key=st.session_state.openai_api_key, 
+        model=st.session_state.model,
+        temperature=0.3,
+        streaming=True )
