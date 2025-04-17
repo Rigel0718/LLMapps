fc06c3811e9ed1d2a612647aa2f754264eac8d66
geonhee <pollux0718@gmail.com>
2025-04-17 23:09:59 +0900

    Add SQLAlchemy model for message storage in sql_db.py

---
+++ sql_db.py
@@ -0,0 +1,17 @@
+from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, Integer, Text
+
+from sqlalchemy import create_engine
+from sqlalchemy.engine import Engine
+
+Base = declarative_base()
+
+def create_message_model(table_name):
+
+    class Messages(Base):
+        __table_name__ = table_name
+        id = Column(Integer, primary_key=True)
+        session_id = Column(Text)
+        message = Column(Text)
+
+    return Messages
