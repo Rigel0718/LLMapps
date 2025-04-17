ca43f70404d25273b3b11b2dc06f5f569e57e5f7
geonhee <pollux0718@gmail.com>
2025-04-17 23:19:31 +0900

    Refactor message model to use Mapped columns and add engine creation for SQLite

--- sql_db.py
+++ sql_db.py
@@ -1,5 +1,5 @@
-from sqlalchemy.orm import declarative_base
-from sqlalchemy import Column, Integer, Text
+from sqlalchemy.orm import declarative_base, Mapped, mapped_column
+from sqlalchemy import Integer, Text
 
 from sqlalchemy import create_engine
 from sqlalchemy.engine import Engine
@@ -10,10 +10,12 @@ def create_message_model(table_name):
 
     class Messages(Base):
         __table_name__ = table_name
-        id = Column(Integer, primary_key=True)
-        session_id = Column(Text)
-        conversation_title = Column(Text)
-        message = Column(Text)
+        id : Mapped[int] = mapped_column(Integer, primary_key=True)
+        session_id : Mapped[str] = mapped_column(Text)
+        conversation_title : Mapped[str] = mapped_column(Text, nullable=True)
+        message : Mapped[str] = mapped_column(Text, nullable=True)
 
     return Messages
+
+engine = create_engine(url='sqlite:///.db', echo=True)
 
