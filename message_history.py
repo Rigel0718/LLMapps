bf05dab74552adc13fbd2e76493540d8b8bafd95
geonhee <pollux0718@gmail.com>
2025-04-22 23:15:01 +0900

    Add type hint for get_message_history_sqlitedb function

--- message_history.py
+++ message_history.py
@@ -1,14 +1,29 @@
 from langchain_community.chat_message_histories import SQLChatMessageHistory
 from langchain_core.runnables.utils import ConfigurableFieldSpec
+from langchain_core.chat_history import BaseChatMessageHistory
 from sql_db import CustomMessageConverter
 
-def get_message_history_sqlitedb(client_id, conversation_num):
+def get_message_history_sqlitedb(client_id, conversation_num) -> BaseChatMessageHistory:
     return SQLChatMessageHistory(
         table_name=client_id,
         session_id=conversation_num,
         connection='sqlite:///custom.db',
         custom_message_converter=CustomMessageConverter(client_id)
     )
+
+
+def load_messages_from_sqlite(client_id: str, conversation_num: str):
+    history = SQLChatMessageHistory(
+        table_name=client_id,
+        session_id=conversation_num,
+        connection='sqlite:///custom.db' 
+    )
+    messages = history.messages
+    
+    return [
+        {"role": "user" if m.type == "human" else "assistant", "content": m.content}
+        for m in messages
+    ]
 
 configs_fields =[
     ConfigurableFieldSpec(
@@ -26,17 +41,3 @@ configs_fields =[
         is_shared=True
     )
 ]
-
-
-def load_messages_from_sqlite(client_id: str, conversation_num: str):
-    history = SQLChatMessageHistory(
-        table_name=client_id,
-        session_id=conversation_num,
-        connection='sqlite:///custom.db' 
-    )
-    messages = history.messages
-    
-    return [
-        {"role": "user" if m.type == "human" else "assistant", "content": m.content}
-        for m in messages
-    ]
