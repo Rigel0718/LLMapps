79115f67947191bd4df5fe38ac2eeb973bc305b1
geonhee <pollux0718@gmail.com>
2025-04-22 19:30:25 +0900

    Add BaseMessageConverter class

--- sql_db.py
+++ sql_db.py
@@ -1,10 +1,12 @@
-from typing import Generator, List, Optional
+from typing import Generator, List, Optional, Any
 from sqlalchemy.orm import declarative_base, Mapped, mapped_column, sessionmaker, Session
 from sqlalchemy import Integer, Text 
 import json
 from contextlib import contextmanager
 from sqlalchemy import create_engine
 from langchain_core.messages import messages_from_dict, BaseMessage
+from abc import ABC, abstractmethod
+
 
 Base = declarative_base()
 
@@ -40,6 +42,30 @@ def messages(session_id) -> List[BaseMessage]:
     for msg in result:
         chat.append(messages_from_dict([json.loads(msg.message)])[0])
     return chat
+
+
+
+# BaseMessageConverter is not packaged in LangChain, so I copied and pasted it directly 
+class BaseMessageConverter(ABC):
+    """Convert BaseMessage to the SQLAlchemy model."""
+
+    @abstractmethod
+    def from_sql_model(self, sql_message: Any) -> BaseMessage:
+        """Convert a SQLAlchemy model to a BaseMessage instance."""
+        raise NotImplementedError
+
+    @abstractmethod
+    def to_sql_model(self, message: BaseMessage, session_id: str) -> Any:
+        """Convert a BaseMessage instance to a SQLAlchemy model."""
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_sql_model_class(self) -> Any:
+        """Get the SQLAlchemy model class."""
+        raise NotImplementedError
+
+
+
 
 if __name__ == '__main__':
     table_name = 'KK'
