581add3b0b6ea6363297b1ac2a91ecb40736475a
geonhee <pollux0718@gmail.com>
2025-04-27 17:39:45 +0900

    Add get_vanilla_chain function to chains.py

---
+++ chains/chains.py
@@ -0,0 +1,10 @@
+from llm import get_OpenAILLM
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+
+
+def get_vanilla_chain(openai_api_key, model_name):
+    prompt_filepath = '.prompts/normal_prompt.yaml'
+    prompt = ChatPromptTemplate(get_chat_prompt_yaml(prompt_filepath))
+
+    return prompt | get_OpenAILLM(openai_api_key, model_name) | StrOutputParser()
