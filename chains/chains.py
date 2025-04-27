2115e665adf5110e0f8089a6db1de830d1467599
geonhee <pollux0718@gmail.com>
2025-04-27 17:42:09 +0900

    Add get_chat_prompt_yaml function to utils.py and import it in chains.py

--- chains/chains.py
+++ chains/chains.py
@@ -1,6 +1,7 @@
 from llm import get_OpenAILLM
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
+from utils import get_chat_prompt_yaml
 
 
 def get_vanilla_chain(openai_api_key, model_name):
---
+++ chains/utils.py
@@ -0,0 +1,7 @@
+import yaml
+
+
+def get_chat_prompt_yaml(file_path):
+    with open(file_path, "r", encoding='utf8') as f:
+        yaml_content = yaml.safe_load(f)
+        return [(message['role'], message['content']) for message in yaml_content['messages']]
