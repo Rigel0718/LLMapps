309e84ac18652a3da15f1b6adfafd3803b21f31a
geonhee <pollux0718@gmail.com>
2025-05-11 17:51:18 +0900

    Add dataloader module with document and web loader exports

---
+++ rag/dataloader/__init__.py
@@ -0,0 +1,9 @@
+from .dataloader import get_documents, get_url_documents
+from .web_dataloader import bs4_web_loader, web_loader
+
+__all__ = [
+    "get_documents",
+    "get_url_documents",
+    "bs4_web_loader",
+    "web_loader",
+]
