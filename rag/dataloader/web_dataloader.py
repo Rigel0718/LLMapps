645510d621e78bd660dfc6abf1da08a5cfce20c9
geonhee <pollux0718@gmail.com>
2025-05-11 17:34:28 +0900

    Add web loader data loading  with Langchain integration

--- rag/dataloader/web_dataloader.py
+++ rag/dataloader/web_dataloader.py
@@ -1,10 +1,13 @@
 import requests
 from bs4 import BeautifulSoup, SoupStrainer
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_community.document_loaders.base import BaseLoader
 
-url = "https://turingpost.co.kr/p/8-rag-master-course"
+url = ""
 headers = {
     "User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36'
 }
+
 
 
 def bs4_web_loader(url):
@@ -12,3 +15,19 @@ def bs4_web_loader(url):
     response.encoding = 'utf-8' 
     soup = BeautifulSoup(response.text, 'html.parser', parse_only=SoupStrainer('div', attrs={'id': ['web-header', 'content-blocks']}))
     return soup.get_text()
+
+
+def web_loader(url) -> BaseLoader:
+    loader = WebBaseLoader(
+    web_path=url,
+    encoding='utf-8',
+    bs_kwargs=dict(
+        parse_only=SoupStrainer(
+            'div',
+            attrs={'id' : ['web-header', 'content-blocks']}
+        )
+    ),
+    header_template=headers
+    )
+    return loader
+    
