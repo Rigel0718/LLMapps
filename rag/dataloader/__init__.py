8a0adf4153c8b547bbb57836c547325b9baf0050
geonhee <pollux0718@gmail.com>
2025-05-13 19:53:06 +0900

    Refactor dataloader module & Custom_Streamlit_FileLoader and update imports

--- rag/dataloader/__init__.py
+++ rag/dataloader/__init__.py
@@ -1,9 +1,7 @@
-from .dataloader import get_documents, get_url_documents, Custom_Streamlit_FileLoader
+from .custom_streamlit_fileloader import Custom_Streamlit_FileLoader
 from .web_dataloader import bs4_web_loader, web_loader
 
 __all__ = [
-    "get_documents",
-    "get_url_documents",
     "bs4_web_loader",
     "web_loader",
     "Custom_Streamlit_FileLoader",
