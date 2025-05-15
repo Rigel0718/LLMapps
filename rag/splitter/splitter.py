14db1f6aad0d4156d4e7fb0618093168d7f27f32
geonhee <pollux0718@gmail.com>
2025-05-15 18:57:07 +0900

    Add build_html_header_splitter function to create HTMLHeaderTextSplitter with specified headers

--- rag/splitter/splitter.py
+++ rag/splitter/splitter.py
@@ -1,5 +1,5 @@
 from langchain.text_splitter import RecursiveCharacterTextSplitter, HTMLHeaderTextSplitter
-from typing import List
+from typing import List, Any, Dict
 from bs4 import BeautifulSoup
 
 
@@ -7,3 +7,10 @@ def get_html_list_tags(soup : BeautifulSoup) -> List[str]:
     list_items = soup.find_all("li")
     list_texts = [li.get_text(strip=True) for li in list_items]
     return list_texts
+
+
+def build_html_header_splitter(
+    headers_to_split_on: Dict[str], 
+    **kwargs: Any
+) -> HTMLHeaderTextSplitter:
+    return HTMLHeaderTextSplitter(headers_to_split_on=headers_to_split_on, **kwargs)
