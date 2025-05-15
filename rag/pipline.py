c8c5deda5c761ecfbe49550f59641d2b4c2256d1
geonhee <pollux0718@gmail.com>
2025-05-15 19:57:37 +0900

    Refactor Custom_RAGPipeline

--- rag/pipline.py
+++ rag/pipline.py
@@ -1,5 +1,4 @@
 from typing import Optional, List, Any
-from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_core.language_models import BaseLanguageModel
 from langchain_core.documents import Document
 from langchain_text_splitters.base import TextSplitter
@@ -7,25 +6,23 @@ from langchain_core.runnables import Runnable
 from .dataloader import Custom_Streamlit_FileLoader, web_loader
 from .vectorstore.vectorstore import load_documents_faiss_vectorsotre
 from .retriever import get_retrievered_documents
-
-
+from langchain_community.document_loaders.base import BaseLoader
+from langchain_core.vectorstores.base import VectorStore
 # TODO splitter, vectorstore(embeddingmodel), retriever 모두 객체화로 customizing할 수 있게 
 class Custom_RAGPipeline:
     def __init__(
         self,
+        data_loader : BaseLoader,
         rag_llm: BaseLanguageModel,
-        splitter: TextSplitter = None,
+        splitter: TextSplitter,
+        vectorstore : VectorStore,
         chunk_size: int = 900,
         chunk_overlap: int = 200,
     ):
         self.rag_llm = rag_llm
-        self.splitter = splitter or RecursiveCharacterTextSplitter(
-            chunk_size=chunk_size,
-            chunk_overlap=chunk_overlap
-        )
-        self.dataloader = Custom_Streamlit_FileLoader(
-            splitter=self.splitter
-        )
+        self.splitter = splitter
+        self.dataloader = data_loader
+        self.vectorstore = vectorstore
 
     def run(
         self,
