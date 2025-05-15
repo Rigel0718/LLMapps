a5b1efc409a7d90b2e7d61664abd27f5cc8c6985
geonhee <pollux0718@gmail.com>
2025-05-15 18:52:31 +0900

    Update get_html_list_tags function to specify return type as List[str]

--- rag/splitter/splitter.py
+++ rag/splitter/splitter.py
@@ -3,7 +3,7 @@ from typing import List
 from bs4 import BeautifulSoup
 
 
-def get_html_list_tags(soup : BeautifulSoup) -> List:
+def get_html_list_tags(soup : BeautifulSoup) -> List[str]:
     list_items = soup.find_all("li")
     list_texts = [li.get_text(strip=True) for li in list_items]
     return list_texts
