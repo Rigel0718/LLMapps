024e4ceba7bbb520e9ce0aa1131bcb4f49162c57
geonhee <pollux0718@gmail.com>
2025-05-15 20:08:01 +0900

    Enhance Custom_RAGPipeline to include web_url_loader for loading documents from URLs

--- rag/pipline.py
+++ rag/pipline.py
@@ -3,25 +3,25 @@ from langchain_core.language_models import BaseLanguageModel
 from langchain_core.documents import Document
 from langchain_text_splitters.base import TextSplitter
 from langchain_core.runnables import Runnable
-from .dataloader import Custom_Streamlit_FileLoader, web_loader
 from .vectorstore.vectorstore import load_documents_faiss_vectorsotre
 from .retriever import get_retrievered_documents
 from langchain_community.document_loaders.base import BaseLoader
 from langchain_core.vectorstores.base import VectorStore
-# TODO splitter, vectorstore(embeddingmodel), retriever 모두 객체화로 customizing할 수 있게 
+
+
 class Custom_RAGPipeline:
     def __init__(
         self,
         data_loader : BaseLoader,
+        web_url_loader : BaseLoader,
         rag_llm: BaseLanguageModel,
         splitter: TextSplitter,
         vectorstore : VectorStore,
-        chunk_size: int = 900,
-        chunk_overlap: int = 200,
     ):
         self.rag_llm = rag_llm
         self.splitter = splitter
         self.dataloader = data_loader
+        self.web_url_loader = web_url_loader
         self.vectorstore = vectorstore
 
     def run(
@@ -35,7 +35,7 @@ class Custom_RAGPipeline:
             documents.extend(self.dataloader.load_multiple_streamlit_files(uploaded_files))
 
         if upload_url:
-            documents.extend(web_loader(upload_url))
+            documents.extend(self.web_url_loader.load(upload_url))
 
         vectorstore = load_documents_faiss_vectorsotre(documents)
         retriever_chain = get_retrievered_documents(vectorstore, self.rag_llm)
