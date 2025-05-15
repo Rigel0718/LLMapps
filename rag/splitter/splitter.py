aee0685b3932ea204a6ec914cf805704bce0e3a2
geonhee <pollux0718@gmail.com>
2025-05-15 18:51:12 +0900

    Add HTML list tag extraction function in splitter module

---
+++ rag/splitter/splitter.py
@@ -0,0 +1,9 @@
+from langchain.text_splitter import RecursiveCharacterTextSplitter, HTMLHeaderTextSplitter
+from typing import List
+from bs4 import BeautifulSoup
+
+
+def get_html_list_tags(soup : BeautifulSoup) -> List:
+    list_items = soup.find_all("li")
+    list_texts = [li.get_text(strip=True) for li in list_items]
+    return list_texts
