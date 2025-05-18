7ee85220cf490e0282f72018c467110bc0136c91
geonhee <pollux0718@gmail.com>
2025-05-18 19:02:10 +0900

    Update embedding.py to define

--- rag/vectorstore/embedding.py
+++ rag/vectorstore/embedding.py
@@ -1,15 +1,16 @@
 import os
-
 from langchain_openai import OpenAIEmbeddings
 from langchain_huggingface.embeddings import HuggingFaceEndpointEmbeddings
+
+OPENAI_EMBEDDING_MODEL = 'text-embedding-3-small'
 
 def get_openai_embedding(openapi_key, model_name):
     return OpenAIEmbeddings(model=model_name, api_key=openapi_key)
 
 huggingface_token = os.environ['HUGGINGFACEHUB_API_TOKEN']
-default_huggingface_model_name = 'intfloat/multilingual-e5-large-instruct'
+DEFAULT_HUGGINGFACE_MODEL_NAME = 'intfloat/multilingual-e5-large-instruct'
 
-def get_huggingface_endpoint_embeddings(huggingface_api_token=huggingface_token, model_name=default_huggingface_model_name):
+def get_huggingface_endpoint_embeddings(huggingface_api_token=huggingface_token, model_name=DEFAULT_HUGGINGFACE_MODEL_NAME):
     return HuggingFaceEndpointEmbeddings(
         model=model_name,
         task='feature-extraction',
